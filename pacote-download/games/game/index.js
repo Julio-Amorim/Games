d15eeb594df34fb984a47feb063d0bc711ee9074
Julio Cesar <juliocesar81632@gmail.com>
2021-09-15 14:28:43 -0300

    upload de arquivos

---
+++ pacote-download/games/game/index.js
@@ -0,0 +1,50 @@
+import { SNAKE_SPEED, update as updateSnake, draw as drawSnake, getSnakeHead, selfCollision as snakeSelfCollision } from './snake/index.js'
+import { draw as drawFood, update as updateFood } from './food/index.js'
+import { gameboard, isOutsideBoard } from './board/index.js';
+
+let lastTimeRender = 0;
+
+function main(currentTime) {
+  if (checkGameOver()) {
+    if(confirm('Game Over',)) {
+      if (confirm("Jogar Novamente")) {
+        window.location.reload();
+      }else{
+        window.location.href = "index.html"
+      }
+    } else {
+      window.requestAnimationFrame(main); 
+    }
+    return;
+}
+
+  window.requestAnimationFrame(main);
+
+  const secondsSinceLastRender = (currentTime - lastTimeRender) / 1000;
+
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
+  lastTimeRender = currentTime;
+
+  update();
+
+  draw();
+}
+
+function update() {
+  updateSnake();
+  updateFood();
+  checkGameOver();
+}
+
+function draw() {
+  gameboard.innerHTML = '';
+  drawSnake(gameboard);
+  drawFood(gameboard);
+}
+
+function checkGameOver() {
+  return isOutsideBoard(getSnakeHead()) || snakeSelfCollision();
+}
+
+window.requestAnimationFrame(main)
